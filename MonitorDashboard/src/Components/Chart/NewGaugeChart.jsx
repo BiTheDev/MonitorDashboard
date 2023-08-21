9e0a3145eb69b429fc385554a255b87807943028
Brian Ho <brian@MacBook-Pro.local>
2023-08-20 23:25:29 -0700

    smooth needle movement complement

--- MonitorDashboard/src/Components/Chart/NewGaugeChart.jsx
+++ MonitorDashboard/src/Components/Chart/NewGaugeChart.jsx
@@ -4,75 +4,6 @@ import { Chart, DoughnutController, ArcElement } from "chart.js";
 
 import { Typography } from "@mui/material";
 
-const gaugeNeedle = {
-  id: "gaugeNeedle",
-  afterDatasetsDraw(chart, args, plugins) {
-    const { ctx, data } = chart;
-
-    ctx.save();
-    const xCenter = chart.getDatasetMeta(0).data[0].x;
-    const yCenter = chart.getDatasetMeta(0).data[0].y;
-    const outerRadius = chart.getDatasetMeta(0).data[0].outerRadius;
-    const innerRadius = chart.getDatasetMeta(0).data[0].innerRadius;
-    const widthSlice = (outerRadius - innerRadius) / 2;
-    const radius = 15;
-    const angle = Math.PI / 180;
-
-    const needleValue = data.datasets[0].needleValue;
-    const dataTotal = data.datasets[0].data.reduce((a, b) => a + b, 0);
-    const circumferenceRotation =
-      (chart.getDatasetMeta(0).data[0].circumference /
-        Math.PI /
-        data.datasets[0].data[0]) *
-      needleValue;
-    ctx.translate(xCenter, yCenter);
-    ctx.rotate(Math.PI * (circumferenceRotation + 1.5));
-
-    // needle
-    ctx.beginPath();
-    ctx.strokeStyle = "#BDC3C7";
-    ctx.fillStyle = "#BDC3C7";
-    ctx.moveTo(0 - radius, 0);
-    ctx.lineTo(0, 0 - innerRadius - widthSlice);
-    ctx.lineTo(0 + radius, 0);
-    ctx.closePath();
-    ctx.stroke();
-    ctx.fill();
-
-    // dot
-    ctx.beginPath();
-    ctx.arc(0, 0, radius, 0, angle * 360, false);
-    ctx.fill();
-
-    ctx.restore();
-  },
-};
-
-// gaugeFlowMeter plugin
-const gaugeFlowMeter = {
-  id: "gaugeFlowMeter",
-  afterDatasetsDraw(chart, args, plugins) {
-    const { ctx, data } = chart;
-
-    ctx.save();
-    const xCenter = chart.getDatasetMeta(0).data[0].x;
-    const yCenter = chart.getDatasetMeta(0).data[0].y;
-    const needleValue = data.datasets[0].needleValue;
-    const circumferenceRotation =
-      (chart.getDatasetMeta(0).data[0].circumference /
-        Math.PI /
-        data.datasets[0].data[0]) *
-      needleValue;
-
-    // flowMeter
-
-    ctx.font = "bold 30px sans-serif";
-    ctx.fillStyle = "black";
-    ctx.textAlign = "center";
-    ctx.fillText(needleValue + "ms", xCenter, yCenter + 45);
-  },
-};
-
 // gaugeLabels plugin
 // const gaugeLabels = {
 //   id: "gaugeLabels",
@@ -102,7 +33,9 @@ const gaugeFlowMeter = {
 //   },
 // };
 
-Chart.register(DoughnutController, ArcElement, gaugeNeedle, gaugeFlowMeter);
+
+
+Chart.register(DoughnutController, ArcElement);
 
 const NewGaugeChart = ({
   initialChartTitle,
@@ -118,6 +51,94 @@ const NewGaugeChart = ({
   const [chartMeasureType, setChartMeasureType] = useState(initialMeasureType);
   const [chartMeasureRange, setChartMeasureRange] =
     useState(initialMeasureRange);
+
+  const gaugeNeedle = {
+    id: "gaugeNeedle",
+    afterDatasetsDraw(chart, args, plugins) {
+      const { ctx, data } = chart;
+
+      ctx.save();
+      const xCenter = chart.getDatasetMeta(0).data[0].x;
+      const yCenter = chart.getDatasetMeta(0).data[0].y;
+      const outerRadius = chart.getDatasetMeta(0).data[0].outerRadius;
+      const innerRadius = chart.getDatasetMeta(0).data[0].innerRadius;
+      const widthSlice = (outerRadius - innerRadius) / 2;
+      const radius = 15;
+      const angle = Math.PI / 180;
+
+      const needleValue = data.datasets[0].needleValue;
+      const dataTotal = data.datasets[0].data.reduce((a, b) => a + b, 0);
+      const circumferenceRotation =
+        (chart.getDatasetMeta(0).data[0].circumference /
+          Math.PI /
+          data.datasets[0].data[0]) *
+        needleValue;
+      ctx.translate(xCenter, yCenter);
+      ctx.rotate(Math.PI * (circumferenceRotation + 1.5));
+
+      // needle
+      ctx.beginPath();
+      ctx.strokeStyle = "#BDC3C7";
+      ctx.fillStyle = "#BDC3C7";
+      ctx.moveTo(0 - radius, 0);
+      ctx.lineTo(0, 0 - innerRadius - widthSlice);
+      ctx.lineTo(0 + radius, 0);
+      ctx.closePath();
+      ctx.stroke();
+      ctx.fill();
+
+      // dot
+      ctx.beginPath();
+      ctx.arc(0, 0, radius, 0, angle * 360, false);
+      ctx.fill();
+
+      ctx.restore();
+    },
+  };
+
+  // gaugeFlowMeter plugin
+  const gaugeFlowMeter = {
+    id: "gaugeFlowMeter",
+    afterDatasetsDraw(chart, args, plugins) {
+      const { ctx, data } = chart;
+
+      ctx.save();
+      const xCenter = chart.getDatasetMeta(0).data[0].x;
+      const yCenter = chart.getDatasetMeta(0).data[0].y;
+      const needleValue = Math.round(data.datasets[0].needleValue);
+      const circumferenceRotation =
+        (chart.getDatasetMeta(0).data[0].circumference /
+          Math.PI /
+          data.datasets[0].data[0]) *
+        needleValue;
+
+      // flowMeter
+
+      ctx.font = "bold 30px sans-serif";
+      ctx.fillStyle = "black";
+      ctx.textAlign = "center";
+      ctx.fillText(needleValue + chartMeasureType, xCenter, yCenter + 45);
+    },
+  };
+
+  const smoothAnimation = (myChart, targetValue, duration) => {
+    let startValue = myChart.data.datasets[0].needleValue;
+    let startTime = null;
+
+    const animate = (timestamp) => {
+      if (!startTime) startTime = timestamp;
+      const progress = Math.min((timestamp - startTime) / duration, 1);
+      myChart.data.datasets[0].needleValue =
+        startValue + (targetValue - startValue) * progress;
+      myChart.update();
+
+      if (progress < 1) {
+        requestAnimationFrame(animate);
+      }
+    };
+
+    requestAnimationFrame(animate);
+  };
 
   useEffect(() => {
     let myChart;
@@ -141,7 +162,7 @@ const NewGaugeChart = ({
         type: "doughnut",
         data,
         options: {
-          aspectRatio: 1.5,
+          aspectRatio: 1.3,
           plugins: {
             legend: {
               display: false,
@@ -149,7 +170,7 @@ const NewGaugeChart = ({
             tooltip: {
               enabled: false,
             },
-          },
+          }
         },
         plugins: [gaugeNeedle, gaugeFlowMeter],
       });
@@ -158,6 +179,10 @@ const NewGaugeChart = ({
     const changeValueRandomly = () => {
       const newValue = Math.round(Math.random() * 100) + 1;
       setChartValue(newValue);
+
+      if (myChart) {
+        smoothAnimation(myChart, newValue, 1000); // Update needle value with animation
+      }
 
       if (myChart) {
         myChart.data.datasets[0].needleValue = newValue; // Update needle value directly in the dataset
